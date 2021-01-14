449fe44c1c1b6eb5b7293917c227949d2ed153ff
Travis Ko <tkospeaks@gmail.com>
2021-01-13 23:34:30 -0800

    Added in inputs for querying in terminal, added functions for handling input, added functions for adding individual members of work team, and function for writing and rendering all information to HTML. Added in aspects to template to help functionality.

--- Main/app.js
+++ Main/app.js
@@ -1,55 +1,130 @@
+// Creating all the constant variables for requirement to generate the employee template //
+// Manager, Engineer and Intern require // 
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 
-
-const util = require("util");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const writeFileAsync = util.promisify(fs.writeFile);
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
+const Choices = require("inquirer/lib/objects/choices");
+const teamList = [];
 
-
-
-//------------------------------------------------------------------
-//Variable for entire array
-const teamArr = [];
-
-// Prompts for Manager
-const managerQuest = [
-    {
-        type: "input",
-        message: "What is the manager's full name?"
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "What is your ID number?"
-        name: "id"
-    },
-];
-
-//------------------------PROMPTS-----------------------------------//
-
-function queryManager() {
-    inquirer
-        .prompt(managerQuest)
-        .then(function (input) {
-            console.log("manager");
-            const manager = new Manager(input.name, input.id)
-            teamArr.push(manager);
-
-            createTeam();
+// Creating a function for adding the manager details card //
+    function addManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is this Manager's name please?",
+                name: "managerName"
+            },
+            {
+                type: "input",
+                message: "What is this Manager's ID number (numeric please)?",
+                name: "managerID"
+            },
+            {
+                type: "input",
+                message: "What is this Manager's email address?",
+                name: "managerEmail"
+            },
+            {
+                type: "input",
+                message: "What is your Manager's office number my friend?",
+                name: "officeNumber"
+            }
+        ]).then(response => {
+            const newManager = new Manager(response.managerName, response.managerID, response.managerEmail, response.officeNumber);
+            teamList.push(newManager);
+            chooseMember();
         });
-};
-
-
+    }
+    function addEngineer() {
+         inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your Engineer's name?",
+                name: "engName"
+            },
+            {
+                type: "input",
+                message: "What is this talented Engineer's ID number (numeric please)?",
+                name: "engID"
+            },
+            {
+                type: "input",
+                message: "What is your Engineer's email address?",
+                name: "engEmail"
+            },
+            {
+                type: "input",
+                message: "What is your Engineer's GitHub username?",
+                name: "github"
+            }
+        ]).then(response => {
+            const newEngineer = new Engineer(response.engName, response.engID, response.engEmail, response.github);
+            teamList.push(newEngineer);
+            chooseMember();
+        });
+    }
+    function addIntern() {
+         inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the Intern's name?",
+                name: "internName"
+            },
+            {
+                type: "input",
+                message: "What is the Intern's ID number?",
+                name: "internID"
+            },
+            {
+                type: "input",
+                message: "What is the Intern's email address?",
+                name: "internEmail"
+            },
+            {
+                type: "input",
+                message: "What is the Intern's school name?",
+                name: "internSchool"
+            }
+        ]).then(response => {
+            const newIntern = new Intern(response.internName, response.internID, response.internEmail, response.internSchool);
+            teamList.push(newIntern);
+            chooseMember();
+        });
+    }
+    function chooseMember() {
+        inquirer.prompt([
+            {
+                type: "checkbox",
+                message: "Please select an employee to add to your team:",
+                name: "role",
+                choices: ["Manager", "Engineer", "Intern", "There is no one else to add, create my website!"]
+            }
+        ]).then(response => {
+            const position = response.role;
+            if (position == "Manager") {
+                addManager();
+            } else if (position == "Engineer") {
+                addEngineer();
+            } else if (position == "Intern") {
+                addIntern();
+            } else if (position == "There is no one else to add, create my website!") {
+                console.log("You're all finished. Check out your awesome work team!!")
+                write();
+            }
+        });
+    }
+    chooseMember();
+function write() {
+    fs.writeFileSync(outputPath, render(teamList));
+}
 
 
 // Write code to use inquirer to gather information about the development team members,
